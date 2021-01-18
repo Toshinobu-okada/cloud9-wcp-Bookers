462f0fe68a244b5e001484ba6e46999329444938
Toshinobu-okada <okacchi_nobu@yahoo.co.jp>
2021-01-18 12:41:38 +0000

    [Finish]課題終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'tops/index'
-  get 'tops/show'
-  get 'tops/new'
-  get 'tops/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tops
   resources :books
